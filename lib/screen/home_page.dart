deffe5ba4b33b6cd1479d00ad5a0f5fd89817bed
foyzur17 <foyzur17@gmail.com>
2022-04-03 14:44:24 +0600

    imitial push

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,24 @@
+
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+
+        Text("This is testing text")
+        
+      ]),
+    );
+  }
+}
